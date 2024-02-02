6acf06b49f90d5ab6ceb658b77f64f2700fc2bf4
dillibabu91GIT <128347745+dillibabu91GIT@users.noreply.github.com>
2024-02-02 13:06:36 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,10 @@ Profile_home="$1"
 zip_file_url="$2"
 zip_file_name="$3"
 
+# Backup directory
+backup_dir="$Profile_home/backup"
+mkdir -p "$backup_dir"
+
 # Remove the existing release directory if it exists
 rm -rf "$Profile_home/release"
 
@@ -28,9 +32,16 @@ if [ -n "$release_content" ]; then
     while IFS=, read -r app_name app_targeted_location; do
         # Check if the app_targeted_location is not empty
         if [ -n "$app_targeted_location" ]; then
-            # Define paths for new deployment files and targeted location
+            # Define paths for new deployment files, targeted location, and backup
             new_deployment_path="$Profile_home/release/$app_name"  # Assuming the new files are in the release directory
             targeted_path="$targeted_location/$app_targeted_location"
+            backup_path="$backup_dir/$app_name"
+
+            # Create a backup of the existing files
+            cp -r "$targeted_path" "$backup_path"
+
+            # Print information about the backup
+            echo "Backup: $app_name - $targeted_path -> $backup_path"
 
             # Copy new deployment files to targeted location
             cp -r "$new_deployment_path" "$targeted_path"
